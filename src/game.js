9aab8785ddb76acc86457cba4023e17d3733fc78
Alex Gonzales <agonz056@ucr.edu>
2014-01-18 12:21:54 -0800

    setup pixi

---
+++ src/game.js
@@ -0,0 +1,35 @@
+
+(function(){
+	var stage = new PIXI.Stage(0x066FF99);
+	var render = PIXI.autoDetectRenderer(400, 300);
+	document.body.appendChild(renderer.view);
+	requestAnimFrame(animate);
+
+    // create a texture from an image path
+    var texture = PIXI.Texture.fromImage("static/bunny.png");
+    // create a new Sprite using the texture
+    var bunny = new PIXI.Sprite(texture);
+
+    // center the sprites anchor point
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    // move the sprite t the center of the screen
+    bunny.position.x = 200;
+    bunny.position.y = 150;
+
+    stage.addChild(bunny);
+
+    function animate() {
+
+        requestAnimFrame( animate );
+
+        // just for fun, lets rotate mr rabbit a little
+        bunny.rotation += 0.1;
+
+        // render the stage
+        renderer.render(stage);
+    }
+
+
+})();
