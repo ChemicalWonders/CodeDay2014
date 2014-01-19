26ac8f02c77ae147d8cd2b66ce945d7479acb09c
ChemicalWonders <kchan039@ucr.edu>
2014-01-18 22:04:30 -0800

    adding other files into mastering

---
+++ src/enemy.js
@@ -0,0 +1,105 @@
+
+//ENEMY CLASS
+//ADDS ITSELF TO THE STAGE
+
+function Enemy(stage, x,y, direction)
+{
+		var texture = PIXI.Texture.fromImage("static/bunny.png");
+		this.object = new PIXI.Sprite(texture);
+
+		this.shootTimer = Date.now();
+		this.object.position.x = x;
+		this.object.position.y = y;
+		this.direction = direction;
+
+		this.stage = stage;
+		this.health = 1;
+		//assign arbitrary hit area
+		this.object.hitArea = new PIXI.Rectangle(0,0,10,10);
+
+		Enemy.allEnemies.push(this);
+		this.addToStage(stage);
+}
+
+
+Enemy.prototype.run = function()
+{
+		//need this function for all types of enemies
+		//try to find the plaayer
+/*
+		if(getDistanceFrom(player) < 500){
+				//shoot at player - do later
+				
+				}
+		else
+				{
+						//move toward player
+						
+				}*/
+		//just move him for now
+		//patrol around origional area
+		if(this.health > 0)
+		{
+				this.object.position.x += 1;
+				if(this.shootTimer < Date.now()) {
+						this.fireProjectile();
+						this.shootTimer = Date.now() + 1000;
+				}
+		}
+		else
+		{
+				//kill
+				//remove from the list
+				this.object.visible = false;
+				delete this;
+		}
+}
+
+Enemy.prototype.addToStage = function(stage)
+{
+		stage.addChild(this.object);
+}
+
+Enemy.prototype.getDistanceFrom = function(player)
+{
+		//get distance from using formulas
+		var distX = player.object.position.x - this.object.position.x;
+		var distY = player.object.position.y - this.object.position.y;
+		var ret = Math.sqrt(distX*distX + distY*distY);
+		return ret;
+}
+
+Enemy.prototype.fireProjectile = function()
+{
+		//create a projectile and add that projectil to the objectmanager
+		//set it on a timer
+		
+		var proj = new Projectile(this.stage,
+															this.object.position.x,
+															this.object.position.y,
+															5,
+															this.direction, 1000);
+
+}
+
+Enemy.prototype.damage = function(modifier)
+{
+		this.health -= modifier;
+}
+
+//Return function
+//to get the bounding box
+Enemy.prototype.getBoundingBox = function()
+{
+		return this.object.hitArea;
+}
+
+//return function
+//to see if specified point is within the collision bounds
+Enemy.prototype.getPointInsideBox = function(x, y)
+{
+		return ((x > this.object.hitArea.x && x < this.object.hitArea.x+this.object.hitArea.width)
+						&& (y > this.object.hitArea.y && y < this.object.hitArea.y+this.object.hitArea.height));
+}
+
+Enemy.allEnemies = new Array();
---
+++ src/projectile.js
@@ -0,0 +1,74 @@
+//PROJECTIEL CLASS
+//DO THIS
+
+function Projectile(stage, x, y, speed, direction, lifetime)
+{
+		//initialize values
+		var texture = PIXI.Texture.fromImage("static/ball.jpg");
+		this.object = new PIXI.Sprite(texture);
+		this.object.position.x = x;
+		this.object.position.y = y;
+		this.speed = speed;
+		this.direction = direction;
+		this.object.hitArea = new PIXI.Rectangle(0,0,1,1);
+		this.stage = stage;
+		Projectile.allProjectiles.push(this);
+		this.addToStage(stage);
+		this.endTimer = Date.now() + lifetime;
+		
+
+}
+
+
+Projectile.prototype.run = function()
+{
+		//shoot in the direction that we were pointing to
+		if(this.endTimer > Date.now())
+		{
+				if(this.direction == -1){
+						this.object.position.x += this.speed;
+				}
+				else if(this.direction == 1){
+						this.object.position.x -= this.speed;
+				}
+				//check for the damaages
+				for(i = 0; i < Enemy.allEnemies.length; i++)
+				{
+						if(Enemy.allEnemies[i].getPointInsideBox(this.object.position.x,
+																										 this.object.position.y))
+						{
+								//hit enemy
+								Enemy.allEnemies[i].damage(1);
+								this.drop();
+						}
+				}
+		}
+		else
+		{
+				this.drop();
+		}
+}
+
+Projectile.prototype.addToStage = function(stage)
+{
+		stage.addChild(this.object);
+}
+
+//self deletion function
+Projectile.prototype.drop = function()
+{
+
+		//kill
+		//make sure we delete from the array
+		var i = Projectile.allProjectiles.indexOf(this);
+		if( i != -1 )
+		{
+				Projectile.allProjectiles.splice(i,1);
+		}
+		this.object.visible = false;
+		delete this;
+}
+
+Projectile.allProjectiles = new Array();
+
+
