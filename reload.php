0d84f7400cccd668d5cb793c80e5c77abcffeae2
claireli <cli024@ucr.edu>
2014-01-19 09:05:53 -0800

    last one!
    claire

---
+++ reload.php
@@ -0,0 +1,19 @@
+<?php 
+
+<script type="text/javascript">
+
+	var x;
+	x = hero.health;
+
+	var life_count = x;
+	var person="<img src='static/heart.png'>";
+
+
+	for (var i=0;i<life_count;i++)
+	{ 
+	document.write(person + "&nbsp;");
+	}
+
+</script>
+
+?>
