18e3b464bcfe9debf26cac52f7cd4ff1aba1a844
Matt Uthainatapong <mnuthai@gmail.com>
2014-01-19 03:34:29 -0800

    adsf

--- src/game.js
+++ src/game.js
@@ -1,17 +1,31 @@
 var main = function(){
+	kd.LEFT.down(function() {
+	  return hero.velocity_x -= 8;
+	});
 
-}
+	kd.RIGHT.down(function() {
+	  return hero.velocity_x += 8;
+	});
 
-var main = function(){
+	kd.X.down(function() {
+	  return hero.velocity_y -= hero.sprite.height/2;
+	});
+
+	kd.run(function() {
+		kd.tick();
+	})
+
+    // Sets up stage, canvas and renderer
 	var stage = new PIXI.Stage(0x66FF99);
 	var canvas = document.getElementById('game-canvas');
 	var renderer = PIXI.autoDetectRenderer(canvas.width, canvas.height, canvas);
+	
+    // Sets up background
 
 	stage.addChild(hero.sprite);
 	renderer.render(stage);
 
 	var farTexture = PIXI.Texture.fromImage("static/cave-background.jpg");	
-
 		far = new PIXI.TilingSprite(farTexture, 1300, 650);
 		far.position.x = 0;
 		far.position.y = 0;
@@ -19,27 +33,7 @@ var main = function(){
 		far.tilePosition.y = 0;
 		stage.addChild(far);
 
-	kd.LEFT.down(function() {
-	  hero.velocity_x -= 8;
-	});
-
-	kd.RIGHT.down(function() {
-	   hero.velocity_x += 8;
-	});
-
-	kd.X.down(function() {
-	  hero.velocity_y -= hero.sprite.height;
-	});
-
-	kd.Z.down(function() {
-	  hero.fire_projectile(stage);
-	});
-
-	kd.run(function() {
-		kd.tick();
-	})
-
-		hero.update();
+        hero.update();
 
         //file doesn't exist it still works. wat
 		var midTexture = PIXI.Texture.fromImage("static/ignore.png");
@@ -49,18 +43,34 @@ var main = function(){
 		mid.tilePosition.x = 0;
         mid.tilePosition.y = 0;
         stage.addChild(mid);
+
+    // Rectangle block 
+    var rectBlock = PIXI.Rectangle(0, 600, 1300, 50);
+    
+    // Tiles/Platforms texture
+    var platTexture = PIXI.Texture.fromImage("static/stoneBlock.jpg");
+        plat = new PIXI.TilingSprite(platTexture, 1300, 50);
+        plat.position.x = 0;
+        plat.position.y = 600;
+        plat.tilePosition.x = 0;
+        plat.tilePosition.y = 0;
+        stage.addChild(plat);
+
         requestAnimFrame(update);
 
+    // Update Function
+    function update() {
+		far.tilePosition.x -= 0.128;
+		mid.tilePosition.x -= 0.64;
+        plat.tilePosition.x -= 0.128;
 		renderer.render(stage);
-		console.log(hero.health);
+        hero.update();
 		objectmanager.run();
-		hero.update();
 		renderer.render(stage);
   		requestAnimFrame(update);
 
 	}
 	var enemy = new Enemy(stage, 200,0,-1);
-	var d = new Enemy(stage, 500,200, -1);
 	var objectmanager = new ObjectManager();
 	stage.addChild(hero.sprite);
 	renderer.render(stage);
@@ -68,5 +78,7 @@ var main = function(){
 
 	requestAnimFrame(update);
 };
+
+
 
 
