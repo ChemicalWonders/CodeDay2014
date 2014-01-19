d35cea2318dd1c1b498c6648d2fa053ddabdf2a1
Benjamin Quach <benquach16@yahoo.com>
2014-01-18 22:46:40 -0800

    integration phase one

--- src/game.js
+++ src/game.js
@@ -17,12 +17,15 @@ var main = function(){
 	var stage = new PIXI.Stage(0x66FF99);
 	var canvas = document.getElementById('game-canvas');
 	var renderer = PIXI.autoDetectRenderer(canvas.width, canvas.height, canvas);
+	var enemy = new Enemy(stage, 200,0,-1);
+	var objectmanager = new ObjectManager();
 	stage.addChild(hero.sprite);
 	renderer.render(stage);
 
 	var update = function(){
 
 		hero.update();
+			objectmanager.run();
 		renderer.render(stage);
   		requestAnimFrame(update);
 	}
