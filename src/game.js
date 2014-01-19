de380887aae30fd58c0d3cf27d4a31e45c167f2e
MNUthai <MNUthai@gmail.com>
2014-01-19 00:24:27 -0800

    changed index.html

--- src/game.js
+++ src/game.js
@@ -14,12 +14,15 @@ var main = function(){
 	kd.run(function() {
 		kd.tick();
 	})
+
+    // Sets up stage, canvas and renderer
 	var stage = new PIXI.Stage(0x66FF99);
 	var canvas = document.getElementById('game-canvas');
 	var renderer = PIXI.autoDetectRenderer(canvas.width, canvas.height, canvas);
 	
+    // Sets up background
 	var farTexture = PIXI.Texture.fromImage("static/cave-background.jpg");	
-		far = new PIXI.TilingSprite(farTexture, 1300, 650);
+		far = new PIXI.TilingSprite(farTexture, 1300, 600);
 		far.position.x = 0;
 		far.position.y = 0;
 		far.tilePosition.x = 0;
@@ -27,18 +30,34 @@ var main = function(){
 		stage.addChild(far);
 
 
-        //file doesn't exist it still works. wat
-		var midTexture = PIXI.Texture.fromImage("static/ignore.png");
-		mid = new PIXI.TilingSprite(midTexture, 512, 256);
-		mid.position.x = 0;
-		mid.position.y = 128;
-		mid.tilePosition.x = 0;
+    //file doesn't exist it still works. wat
+    var midTexture = PIXI.Texture.fromImage("static/ignore.png");
+        mid = new PIXI.TilingSprite(midTexture, 0, 0);
+        mid.position.x = 0;
+        mid.position.y = 0;
+        mid.tilePosition.x = 0;
         mid.tilePosition.y = 0;
         stage.addChild(mid);
+
+    // Rectangle block 
+    var rectBlock = PIXI.Rectangle(0, 600, 1300, 50);
+    
+    // Tiles/Platforms texture
+    var platTexture = PIXI.Texture.fromImage("static/tiles.png");
+        plat = new PIXI.TilingSprite(platTexture, 1300, 50);
+        plat.position.x = 0;
+        plat.position.y = 600;
+        plat.tilePosition.x = 0;
+        plat.tilePosition.y = 0;
+        stage.addChild(plat);
+
         requestAnimFrame(update);
+
+    // Update Function
     function update() {
 		far.tilePosition.x -= 0.128;
 		mid.tilePosition.x -= 0.64;
+        plat.tilePosition.x -= 0.128;
 		renderer.render(stage);
         hero.update();
 		objectmanager.run();
