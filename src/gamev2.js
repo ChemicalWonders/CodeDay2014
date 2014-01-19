1c997d2b06db977db760e521dab9e6ad3f28a0d2
Alex Gonzales <agonz056@ucr.edu>
2014-01-18 20:42:52 -0800

    Revert "updated"
    This reverts commit 9c07006c19a513f40013d19cfa930c18e0de25df.
    
    reverted

---
+++ src/gamev2.js
@@ -0,0 +1,42 @@
+
+(function(){
+	var stage = new PIXI.Stage(0x066FF99);
+	var renderer = PIXI.autoDetectRenderer(1500, 625);
+	document.body.appendChild(renderer.view);
+	requestAnimFrame(animate);
+
+    // create a texture from an image path
+    var texture = PIXI.Texture.fromImage("static/bunny.png");
+
+		var person = new Player(0,0);
+		var objectmanager = new ObjectManager();
+		var enemy = new Enemy(stage, 200,0, 1);
+
+
+		person.addToStage(stage);
+		enemy.addToStage(stage);
+    function animate() {
+
+        requestAnimFrame( animate );
+				objectmanager.run();
+        document.addEventListener('keydown',function(event){
+            if(event.keyCode == 37 || event.keyCode == 65) {
+                bunny.position.x -= 0.005;
+            }
+
+            if (event.keyCode == 39 || event.keyCode == 68){
+                bunny.position.x += 0.005;
+            }
+
+            if (event.keyCode == 40 || event.keyCode == 83)
+                bunny.position.y +=0.005;
+
+            if (event.keyCode == 38 || event.keyCode == 87)
+                bunny.position.y -=0.005;
+        });
+
+        // render the stage
+        renderer.render(stage);
+    }
+    animate();
+})();
